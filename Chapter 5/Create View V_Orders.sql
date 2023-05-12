2fd5afb44aa00240ed494d46ebbfd520003df95c
Nikiya Simpson <nsimpson@uams.edu>
2023-05-12 18:10:44 +0000

    Update Chapter 5

---
+++ Chapter 5/Create View V_Orders.sql
@@ -0,0 +1,10 @@
+DROP VIEW V_Orders;
+CREATE VIEW V_Orders AS
+SELECT O.CreationDate AS OrderDate,
+  CONCAT(S.FirstName, ' ', S.LastName) AS Salesperson,
+  COUNT(1) AS 'Amount of Orders',
+  SUM(TotalDue) AS 'Total Due'
+FROM Orders O
+  join Salesperson S ON(O.SalespersonID = S.SalespersonID)
+GROUP BY O.CreationDate,
+  concat(S.FirstName, ' ', S.LastName)
