0bd6064ab218da7a06208bc4c61d436740752dd5
Nikiya Simpson <nikiyasimpson@gmail.com>
2023-09-11 16:53:50 -0500

    Update startup.sh
    revert changes

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,12 +1,9 @@
-# Set up the initial database
+# Run an initial setup script for the MariaDB database
+if [ -f .devcontainer/setup-mariadb.sql ]; then
+  sleep 10
+  mysql -h 127.0.0.1 -uroot -pmariadb < .devcontainer/setup-mariadb.sql
+fi
 
-for i in $(seq 1 30); do
-      if mysql -h 127.0.0.1 -uroot -pmariadb -e ";" ; then
-            mysql -h 127.0.0.1 -uroot -pmariadb < .devcontainer/setup-mariadb.sql &&
-            mysql -h 127.0.0.1 -uroot -pmariadb < .devcontainer/H_Plus_Sports_MySQL.sql &&
-            echo "Database is set up" &&
-            break
-      fi
-      sleep 1
-      echo Waiting for db: $i
-done
+if [ -f .devcontainer/H_Plus_Sports_MySQL.sql ]; then
+  mysql -h 127.0.0.1 -umariadb -pmariadb < .devcontainer/H_Plus_Sports_MySQL.sql
+fi
