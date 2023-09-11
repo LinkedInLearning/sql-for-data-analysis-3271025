ca7fd77c8f71488b21612cf87fa27ddd7e54f7ae
Nikiya Simpson <nikiyasimpson@gmail.com>
2023-09-11 17:07:34 -0500

    Update startup.sh
    update sleep parameters

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,10 +1,10 @@
 # Run an initial setup script for the MariaDB database
 if [ -f .devcontainer/setup-mariadb.sql ]; then
-  sleep 10
+  sleep 20
   mysql -h 127.0.0.1 -uroot -pmariadb < .devcontainer/setup-mariadb.sql
 fi
 
 if [ -f .devcontainer/H_Plus_Sports_MySQL.sql ]; then
-  sleep 30
+  sleep 20
   mysql -h 127.0.0.1 -umariadb -pmariadb < .devcontainer/H_Plus_Sports_MySQL.sql
 fi
