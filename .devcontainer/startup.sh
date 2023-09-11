f2554a6d2caebe8d50d1acdd2db80a310a1dd2ef
Nikiya Simpson <nikiyasimpson@gmail.com>
2023-09-11 16:48:45 -0500

    Update startup.sh
    Update startup script that tries for 30 seconds to connect to the database

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,9 +1,12 @@
-# Run an initial setup script for the MariaDB database
-if [ -f .devcontainer/setup-mariadb.sql ]; then
-  sleep 10
-  mysql -h 127.0.0.1 -uroot -pmariadb < .devcontainer/setup-mariadb.sql
-fi
+# Set up the initial database
 
-if [ -f .devcontainer/H_Plus_Sports_MySQL.sql ]; then
-  mysql -h 127.0.0.1 -umariadb -pmariadb < .devcontainer/H_Plus_Sports_MySQL.sql
-fi
+for i in $(seq 1 30); do
+      if mysql -h 127.0.0.1 -uroot -pmariadb -e ";" ; then
+            mysql -h 127.0.0.1 -uroot -pmariadb < .devcontainer/setup-mariadb.sql &&
+            mysql -h 127.0.0.1 -uroot -pmariadb < .devcontainer/H_Plus_Sports_MySQL.sql &&
+            echo "Database is set up" &&
+            break
+      fi
+      sleep 1
+      echo Waiting for db: $i
+done
